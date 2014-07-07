ff0c318633304d750b5782d928a58388c8bf2c08
Alparslan Kapani <alparslankapani@gmail.com>
2014-07-07 20:38:47 +0300

    Rearrange Pin Controller(correct user parameter)

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -46,10 +46,10 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin = current_user.pins.find_by(params[:id])
-      redirect_to pins_path, notice: 'Not authorised to edit the path' if @pin.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
-
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
